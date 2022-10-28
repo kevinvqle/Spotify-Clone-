2aea9c328c3ba9c487daedf2c3398082a908dbc0
Jameboyyy <jayboysteelers04@yahoo.com>
2022-10-27 17:35:27 -0700

    Button Changes

--- spotify-clone/src/ui/SignUpNav.jsx
+++ spotify-clone/src/ui/SignUpNav.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { HashLink } from 'react-router-hash-link';
 
 const signUpNav = () => {
     return (
@@ -33,9 +34,21 @@ const signUpNav = () => {
                     <input type="text" placeholder='Enter a profile name'/><br/>
                     <label htmlFor="date">What's your date of birth?</label><br/>
                     <input type="date" placeholder='MM/DD/YYYY'/>
-                    <label>What's your gender?</label>
-                    <input type='radio'/>
-                    <label>Male</label>     
+                    <label htmlFor='genderOpt'>What's your gender?</label> 
+                    <div className="radio__opt">
+                        <div className="radio__sec">
+                            <input type='radio'></input> 
+                            <label htmlFor='lab__opt'>Male</label>
+                        </div>
+                        <div className="radio__sec">
+                            <input type='radio'></input> 
+                            <label htmlFor='lab__opt'>Female</label>
+                        </div>
+                    </div>
+                    <button className="signup__btn">Sign Up</button>
+                    <div className="have">
+                        <p className='have__para'>Already have an account?</p>
+                    </div>
                 </form>
             </div>
         </section>
