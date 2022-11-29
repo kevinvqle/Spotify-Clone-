0608ef07680e35f6e401b4709581dd1a222a9af9
MagicHobo2015 <joshua.land6@gmail.com>
2022-11-28 16:04:59 -0800

    added the ability to download the mp3's, for usage see me

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 // adding this for search
 const searchRoutes =  require("./routes/search_function");
+// this is to get the music
+const musicRoutes = require("./routes/music");
 
 const app = express();
 
@@ -21,6 +23,9 @@ app.use("/api/login/",authRoutes);
 
 // this is for searching it just catches the request
 app.get('/search/:artistOrSong', searchRoutes);
+
+// this is for the music it sends you on over to the music.js file in the routes folder
+app.get('/music/:objId', musicRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
