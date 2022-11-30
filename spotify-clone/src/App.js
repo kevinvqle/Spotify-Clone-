431c7cb0533fa7641c8a0ce29b8f28bfd43d8d9c
“Nebil <nebilsolomon@csu.fullerton.edu>
2022-11-29 17:28:32 -0800

    music play and stop

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import LoginPage from './creation/LoginPage';
 import SignUp from './creation/SignUp';
-import { Routes, Route} from "react-router-dom";
+// import { Routes, Route} from "react-router-dom";
 
 
 
 const App = () => {
     return (
         <div className="App">
-            <Routes>
+           
+           
+           <h1>nebil</h1>
+           
+            {/* <Routes>
                 <Route path="/" exact element={ <LoginPage/> } />
                 <Route path="/signup" exact element ={ <SignUp/>} />
-            </Routes>
+            </Routes> */}
         </div>
     );
 }
